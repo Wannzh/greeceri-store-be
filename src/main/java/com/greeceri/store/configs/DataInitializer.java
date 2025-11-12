491d36079a570c713c335560aad548335942ea76
Wannzh <alwanfdhlrhmn@gmail.com>
2025-11-13 03:09:08 +0700

    Add Data Init

--- src/main/java/com/greeceri/store/configs/DataInitializer.java
+++ src/main/java/com/greeceri/store/configs/DataInitializer.java
@@ -1,12 +1,50 @@
 package com.greeceri.store.configs;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Component
-public class DataInitializer implements CommandLineRunner {
-    @Override
-    public void run(String... args) throws Exception {
+import com.greeceri.store.models.entity.User;
+import com.greeceri.store.models.enums.Role;
+import com.greeceri.store.repositories.UserRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+@Slf4j
+public class DataInitializer{
+
+    @Value("${ADMIN_EMAIL}")
+    private String adminEmail;
+
+    @Value("${ADMIN_PASSWORD}")
+    private String adminPassword;
+
+    @Value("${ADMIN_USERNAME}")
+    private String adminUsername;
+
+    @Bean
+    CommandLineRunner initData(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (userRepository.findByEmail(adminEmail).isEmpty()) {
+                User adminUser = User.builder()
+                        .name(adminUsername)
+                        .email(adminEmail)
+                        .password(passwordEncoder.encode(adminPassword))
+                        .role(Role.ADMIN)
+                        .enabled(true)
+                        .build();
+                userRepository.save(adminUser);
+                log.info("Admin user created with email: {}", adminEmail);
+            } else {
+                log.info("Admin user already exists with email: {}", adminEmail);
+            }
+        };
         
     }
+
+
 }
