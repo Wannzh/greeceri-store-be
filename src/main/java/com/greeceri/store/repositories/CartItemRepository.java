4850f3b285fc4551cf1ff4c5f6ecdcc0bf583b10
Wannzh <alwanfdhlrhmn@gmail.com>
2025-11-15 02:14:37 +0700

    add method

--- src/main/java/com/greeceri/store/repositories/CartItemRepository.java
+++ src/main/java/com/greeceri/store/repositories/CartItemRepository.java
@@ -1,9 +1,13 @@
 package com.greeceri.store.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.greeceri.store.models.entity.Cart;
 import com.greeceri.store.models.entity.CartItem;
+import com.greeceri.store.models.entity.Product;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-
+    Optional<CartItem> findByCartAndProduct(Cart cart, Product product);
 }
--- src/main/java/com/greeceri/store/repositories/CartRepository.java
+++ src/main/java/com/greeceri/store/repositories/CartRepository.java
@@ -1,9 +1,12 @@
 package com.greeceri.store.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.greeceri.store.models.entity.Cart;
+import com.greeceri.store.models.entity.User;
 
 public interface CartRepository extends JpaRepository<Cart, String> {
-
+    Optional<Cart> findByUser(User user);
 }
