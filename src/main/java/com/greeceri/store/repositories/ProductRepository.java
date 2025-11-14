c8e3c75284560bc053ba47095bb527d1ea41776c
Wannzh <alwanfdhlrhmn@gmail.com>
2025-11-15 00:08:59 +0700

    add repository product & category

---
+++ src/main/java/com/greeceri/store/repositories/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.greeceri.store.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.greeceri.store.models.entity.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+    List<Product> findByCategory(Long categoryId);
+
+}
