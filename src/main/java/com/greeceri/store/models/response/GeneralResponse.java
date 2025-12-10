32dcf25090f2b2c14fb2803e1727da997a737497
Wannzh <alwanfdhlrhmn@gmail.com>
2025-12-10 17:29:00 +0700

    Add Generic and General Response

---
+++ src/main/java/com/greeceri/store/models/response/GeneralResponse.java
@@ -0,0 +1,13 @@
+package com.greeceri.store.models.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GeneralResponse {
+    private boolean success;
+    private String message;
+}
